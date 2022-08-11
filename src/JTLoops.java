500a868816482ddce3b4f7c7e35b0b091cb74956
hindpatel <hindpatel@yahoo.com>
2022-08-11 17:57:12 -0400

    Updated Loop file.

--- src/JTLoops.java
+++ src/JTLoops.java
@@ -1,2 +1,14 @@
 public class JTLoops {
+
+    public static void main(String[] args) {
+
+        //int i = 1; // we used it just for do while and while loop.
+
+       // while(true) // while loop first checks the condition and  looks for "true" value, 1 is less than 10.
+        //do // do while loop first executes the statement and then checks the condition.
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
 }
